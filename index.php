3c93ea0fd74cc17e23f8742d4e3206e67c346282
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-10-28 23:50:49 -0400

    updated css

--- index.php
+++ index.php
@@ -134,9 +134,7 @@ include 'includes/nav.html';
 <!-- ------------------Sign up------------------     -->
 
     <div class="container_full_dark form-group" style="text-align: center;">
-    <div class="img-wrapper">
-				<img src=" https://i.pinimg.com/originals/9a/60/52/9a60525a400e020726ccdc5606e27201.jpg" style="width:300px"  />
-			</div>
+   
   
       <div>
         <h4 >Let's Connect</h4>
