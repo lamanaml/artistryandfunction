f20821985ed72f377b6882ccbae491ba7157bee3
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-10-31 13:42:56 -0400

    updated navbar

--- index.php
+++ index.php
@@ -117,12 +117,13 @@ include 'includes/nav.html';
                 <h5 class="card-title">Our Experience</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               </div>
-              <div class="card-body">
-                <h5 class="card-title">Our team</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              </div>
             </div>
-          </div> 
+            <div class="card-body">
+              <h5 class="card-title">Our team</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div> 
       </div>
    
       <div class="col mb-4">
