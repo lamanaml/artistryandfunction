7b8b85cbfa07f2d00862da56ea269ca9917e9d6a
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-01 08:11:21 -0500

    updated navbar

--- index.php
+++ index.php
@@ -134,7 +134,7 @@ include 'includes/nav.html';
 
 
 <!-- ------------------Sign up------------------     -->
-    <div class="container_full_dark ">
+    <div class="footer1">
       
         <h5 >Subscribe to get tips to increase traffic and revenue!</h5>
         <form class="form-inline ">
