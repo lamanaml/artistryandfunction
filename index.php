685d532e92b2d16db8574593e32109c47d29afec
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-01 07:22:39 -0500

    updated navbar

--- index.php
+++ index.php
@@ -43,10 +43,9 @@ include 'includes/nav.html';
 <!-- ------------------Hero------------------     -->
 
 <div class="hero-image">
- 
-    <h1>Earn even more revenue from your web presence!</h1>
-     <div class="hero-text">
-    <h3>Tell us about your business</h3>
+    <div class="hero-text">
+    <h1>Let us help you figure out the web.  Whether you need a web site, need more traffic, or need more money from your site... we can help. </h1>
+      <h3>Let's face it, most business owners don't have time to build a successful web presence.  That's where we come in!  We will create professional web sites that rank higher in search engine results, getting more people to your website so that you can increase your revenue. This leaves you more time to run your business.  </h3>
      <a href="clientsurvey.php"> <button  class="animate__animated animate__fadeInLeft"> Get Started</button></a>
   </div>
 </div>
