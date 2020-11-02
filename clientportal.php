2c75f8c45c0c20973cf4f4222a66a29ebf66603f
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-01 22:05:48 -0500

    updated cards

--- clientportal.php
+++ clientportal.php
@@ -55,7 +55,10 @@ include 'includes/nav.html';
   <div class="flex-container">
 
   <div class="card2">
-    <img class="card-img-top" src="https://images.yourstory.com/cs/1/16ca2180-5042-11e9-8207-218120872911/digital-payments-pci-ppi-rbi-770x4331554380500502.png" alt="Card image cap">
+    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-credit-card-2-back-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5H0V4zm11.5 1a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-2z"/>
+  <path d="M0 11v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-1H0z"/>
+</svg>
     <span class="card-body">
       <h5 class="card-title">Make a payment</h5>
       <p class="card-text">Pay your upcoming invoice</p>
